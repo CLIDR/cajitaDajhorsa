4caf811c1955f560c293a98859642bb400343671
Clider <clider.romani2016@gmail.com>
2024-11-16 17:56:59 -0500

    create first tables

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'company_num',
+        'company_ruc',
+        'company_name',
+        'status',
+    ];
 }
--- database/migrations/2024_11_14_180834_create_companies_table.php
+++ database/migrations/2024_11_14_180834_create_companies_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('company_num', 1)->nullable();
+            $table->char('company_ruc', 20);
+            $table->string('company_name');
+            $table->boolean('status')->default(true);
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
